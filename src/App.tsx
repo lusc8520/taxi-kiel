aa0dced2f57357bacf933a6c4b12d050f708e76d
lusc8520 <lukas-schuenemann@web.de>
2024-07-19 23:34:34 +0200

    Add links

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
+import {CSSProperties} from "react";
 
 function App() {
 
-
   return (
-    <div>do something</div>
+      <div style={rootStyle}>
+        <div style={titleStyle}>Taxi Kiel</div>
+
+          <a href="mailto: examplename@web.de">
+              <div>
+                  examplename@web.de
+              </div>
+          </a>
+
+          <a href="tel: 123">
+              <button>Click here to call</button>
+          </a>
+        <div>Tel</div>
+      </div>
   )
+}
+
+const titleStyle: CSSProperties = {
+  lineHeight: "1",
+  fontSize: "xx-large"
+}
+
+const rootStyle: CSSProperties = {
+  display: "flex",
+  flexDirection : "column",
+  gap: "1rem",
+  height: "100svh",
+  width: "100svw",
+  boxSizing: "border-box",
+  backgroundColor: "#020419",
+  color: "white",
+  alignItems: "center",
+  padding: "0.5rem",
+  fontFamily: "sans-serif",
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,6 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+document.body.style.margin = "0"
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<App/>)
