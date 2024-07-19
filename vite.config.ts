735ca4e65a44d6f3aca3adcd911bcb85e761dd20
lusc8520 <lukas-schuenemann@web.de>
2024-07-19 20:33:05 +0200

    Test deploy 2

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://lusc8520.github.io/taxi-kiel/dist"
+  base: "https://lusc8520.github.io/taxi-kiel/dist/"
 })
